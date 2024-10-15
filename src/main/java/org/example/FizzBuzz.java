649caf6f44a9ec126615e6fb702730f234971538
Gerald2309 <gerald.e2309@gmail.com>
2024-10-14 23:24:40 -0500

    first commit

---
+++ src/main/java/org/example/FizzBuzz.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Game {
+
+    public static void game(int limit){
+
+        for(int i=1; i<limit; i++){
+
+            if(i % 3 == 0){
+                System.out.println("Fizz");
+
+            } else if(i % 5 == 0){
+                System.out.println("Buzz");
+
+            }
+            else{
+                System.out.println(i);
+            }
+
+
+        }
+
+
+
+
+    }
+
+}
